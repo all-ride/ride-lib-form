2ec41e14e7caf758204ff75c19719749eedcaf0d
Joris Vandeweerd <joris@statik.be>
2014-04-10 02:42:11 +0200

    added button row

---
+++ src/ride/library/form/row/ButtonRow.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace ride\library\form\row;
+
+/**
+ * Button row
+ */
+class ButtonRow extends AbstractRow {
+
+    /**
+     * Name of the row type
+     * @var string
+     */
+    const TYPE = 'button';
+
+}

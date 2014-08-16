b680ff7057671abf61d4f81c89da8242f7e5d1c8
Joris Vandeweerd <joris@statik.be>
2014-08-16 12:58:00 +0200

    extended time row to also work for times with seconds, or just minutes and seconds

--- src/ride/library/form/row/TimeRow.php
+++ src/ride/library/form/row/TimeRow.php
@@ -17,6 +17,18 @@ class TimeRow extends AbstractRow {
     const TYPE = 'time';
 
     /**
+     * Option to include seconds
+     * @var string
+     */
+    const OPTION_SECONDS = 'seconds';
+
+    /**
+     * Option to include hours
+     * @var string
+     */
+    const OPTION_HOURS = 'hours';
+
+    /**
      * Sets the data to this row
      * @param mixed $data
      * @return null
@@ -24,9 +36,22 @@ class TimeRow extends AbstractRow {
     public function setData($data) {
         if (is_numeric($data)) {
             $hours = floor($data / 3600);
-            $minutes = floor(($data % 3600) / 60);
+            $remaining = $data % 3600;
+            $minutes = floor($remaining / 60);
 
-            $data = $hours . ':' . str_pad($minutes, 2, '0');
+            if ($this->getOption(self::OPTION_HOURS, true)) {
+                $data = $hours . ':' . str_pad($minutes, 2, '0', STR_PAD_LEFT);
+            } else {
+                $minutes += $hours * 60;
+
+                $data = $minutes;
+            }
+
+            if ($this->getOption(self::OPTION_SECONDS)) {
+                $seconds = $remaining % 60;
+
+                $data .= ':' . str_pad($seconds, 2, '0', STR_PAD_LEFT);
+            }
         }
 
         parent::setData($data);
@@ -41,9 +66,21 @@ class TimeRow extends AbstractRow {
             return null;
         }
 
-        list($hours, $minutes) = explode(':', $this->data);
+        $time = 0;
 
-        $time = ($hours * 3600) + ($minutes * 60);
+        $tokens = explode(':', $this->data);
+
+        if ($this->getOption(self::OPTION_HOURS, true)) {
+            $time = ($tokens[0] * 3600) + ($tokens[1] * 60);
+            if ($this->getOption(self::OPTION_SECONDS)) {
+                $time += $tokens[2];
+            }
+        } else {
+            $time = $tokens[0] * 60;
+            if ($this->getOption(self::OPTION_SECONDS)) {
+                $time += $tokens[1];
+            }
+        }
 
         return $time;
     }
@@ -56,9 +93,42 @@ class TimeRow extends AbstractRow {
      * @return null
      */
     public function buildRow($namePrefix, $idPrefix, ValidationFactory $validationFactory) {
+        $showHours = $this->getOption(self::OPTION_HOURS, true);
+        $showSeconds = $this->getOption(self::OPTION_SECONDS);
+
+        $placeHolder = '00';
+        $regex = '';
+
+        if ($showHours) {
+            $placeHolder .= ':00';
+            $regex .= '((([0-1])?[0-9])|(2[0-4]))';
+            $regex .= ':[0-5][0-9]';
+        } else {
+            $regex .= '([0-9])*';
+        }
+
+        if ($showSeconds) {
+            $placeHolder .= ':00';
+            $regex . ':[0-5][0-9]';
+        }
+
+        $attributes = $this->getOption(self::OPTION_ATTRIBUTES, array());
+
+        if (isset($attributes['class'])) {
+            $attributes['class'] .= ' time';
+        } else {
+            $attributes['class'] = 'time';
+        }
+
+        if (!isset($attributes['placeholder'])) {
+            $attributes['placeholder'] = $placeHolder;
+        }
+
+        $this->setOption(self::OPTION_ATTRIBUTES, $attributes);
+
         $this->validators[] = $validationFactory->createValidator('regex', array(
             'required' => false,
-            'regex' => '/([0-2])?[0-9]:[0-5][0-9]/',
+            'regex' => '/' . $regex . '/',
             'error.regex' => 'error.validation.time',
         ));
 

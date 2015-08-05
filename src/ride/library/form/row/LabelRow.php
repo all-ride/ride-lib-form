e0330a2c869b90252de2f2c8d055f9c34f0e2278
Kaya <kayalion@kayalion.net>
2015-08-05 17:02:23 +0200

    added decorator to label

--- src/ride/library/form/row/LabelRow.php
+++ src/ride/library/form/row/LabelRow.php
@@ -20,12 +20,34 @@ class LabelRow extends AbstractRow {
     const OPTION_HTML = 'html';
 
     /**
+     * Option for the label decorator
+     * @var string
+     */
+    const OPTION_DECORATOR = 'decorator';
+
+    /**
      * Processes the request and updates the data of this row
      * @param array $values Submitted values
      * @return null
      */
     public function processData(array $values) {
 
+    }
+
+    /**
+     * Creates the widget for this row
+     * @param string $name
+     * @param mixed $default
+     * @param array $attributes
+     * @return \ride\library\form\widget\Widget
+     */
+    protected function createWidget($name, $default, array $attributes) {
+        $decorator = $this->getOption(self::OPTION_DECORATOR);
+        if ($decorator) {
+            $default = $decorator->decorate($default);
+        }
+
+        return parent::createWidget($name, $default, $attributes);
     }
 
 }
